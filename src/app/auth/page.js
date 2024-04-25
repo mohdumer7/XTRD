90cb57afe9a73910049af1b3f7c125eb36b67e52
Mohammedumer7 <mohammed@inspiritvr.com>
2024-04-26 03:10:19 +0530

    domain sl

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -109,7 +109,7 @@ export default function Authentication() {
         //   });
         // }
 
-        const response = await fetch("http://localhost:3000/api/user", {
+        const response = await fetch("https://xtrd.vercel.app/api/user", {
           method: "POST",
           body: JSON.stringify({
             email,
